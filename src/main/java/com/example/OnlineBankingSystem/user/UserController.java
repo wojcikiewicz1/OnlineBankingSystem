47add5eb89ca3a9ebf2c3ab7d5f6a138893bbc60
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2023-11-30 13:01:15 +0100

    commit

--- src/main/java/com/example/OnlineBankingSystem/user/UserController.java
+++ src/main/java/com/example/OnlineBankingSystem/user/UserController.java
@@ -3,7 +3,6 @@ package com.example.OnlineBankingSystem.user;
 import com.example.OnlineBankingSystem.account.CheckingAccount;
 import com.example.OnlineBankingSystem.account.SavingsAccount;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
